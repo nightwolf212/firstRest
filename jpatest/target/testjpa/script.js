121783f99a0aca07d8c523960aa00a2d1370dba2
nightwolf212 <nightwolf212@mail.ru>
2015-02-24 23:06:58 +0800

    Target Deleted

--- jpatest/target/testjpa/script.js
+++
@@ -1,8 +0,0 @@
-function myF($scope) 
-{
- var responseData;
-    $http.get('http://localhost:8080/simple-rest/greeting').then(function(response){
-        responseData = response.data;
-    });
-$scope.data='12';
-}
